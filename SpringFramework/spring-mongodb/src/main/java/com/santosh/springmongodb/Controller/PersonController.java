8e245c7af45b5b98af27d479f03234f9d0e187c9
santosh timilsina <santos.timil@gmail.com>
2023-07-19 00:41:55 +0545

    swagger-using-spring-fox

--- SpringFramework/spring-mongodb/src/main/java/com/santosh/springmongodb/Controller/PersonController.java
+++ SpringFramework/spring-mongodb/src/main/java/com/santosh/springmongodb/Controller/PersonController.java
@@ -3,6 +3,7 @@ package com.santosh.springmongodb.Controller;
 import com.santosh.springmongodb.Services.PersonService;
 import com.santosh.springmongodb.collections.Person;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class PersonController {
 
     private final PersonService personService;
 
+    @PostMapping
     public String save(@RequestBody Person person){
       return   personService.save(person);
     }
--- SpringFramework/spring-mongodb/src/main/java/com/santosh/springmongodb/collections/Address.java
+++ SpringFramework/spring-mongodb/src/main/java/com/santosh/springmongodb/collections/Address.java
@@ -1,5 +1,14 @@
 package com.santosh.springmongodb.collections;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Address {
 
     private String address1;
